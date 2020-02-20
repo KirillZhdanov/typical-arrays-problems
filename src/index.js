3e92dfc37827ed39be403dd377d0cb435676ce12
KirillZh <kirill.zhdanov97@gmail.com>
2020-02-20 12:52:12 +0300

    fix: Works with arrays

--- src/index.js
+++ src/index.js
@@ -1,36 +1,53 @@
 
 exports.min = function min (array) {
-  let minValue=Number.MAX_VALUE,res;
-   
-    for (var i = 0; i < arguments.length; i++) {
-      if(minValue>arguments[i])
-      minValue=arguments[i]
+ let sum=0,res;
+    if(array!=undefined){
+      for (let i = 0; i < arguments.length; i++) {
+      sum+= arguments[i];
     }
-    res=minValue;
-    Math.round(res)==Math.round(Number.MAX_VALUE)?res=0:res;
-    console.log("min value is "+res);
+    
+    for (let i = 0; i < array.length; i++) {
+      sum+= array[i];
+    }}
+    res=sum/arguments.length;
+    console.log("avg value is "+res);
     return res;
 }
 
 exports.max = function max (array) {
   let maxValue=Number.MIN_VALUE,res;
-   
-    for (var i = 0; i < arguments.length; i++) {
+    if(array!=undefined){
+    for (let i = 0; i < arguments.length; i++) {
       if(maxValue<arguments[i])
       maxValue=arguments[i]
     }
-    res=maxValue;
+    for (let i = 0; i < array.length; i++) {
+      if(maxValue<array[i])
+      maxValue=array[i]
+    }
+  } 
+    res=0;
+    res+=maxValue;
     Math.round(res)==Math.round(Number.MIN_VALUE)?res=0:res;
     console.log("max value is "+res);
     return res;
 }
 
 exports.avg = function avg (array) {
-  let sum=0,res;
-    for (var i = 0; i < arguments.length; i++) {
-      sum+= arguments[i];
+ function min(array) {
+    let minValue=Number.MAX_VALUE,res;
+    if(array!=undefined){
+    for (let i = 0; i < arguments.length; i++) {
+      if(minValue>arguments[i])
+      minValue=arguments[i]
     }
-    res=sum/arguments.length;
-    console.log("avg value is "+res);
+    for (let i = 0; i < array.length; i++) {
+      if(minValue>array[i])
+      minValue=array[i]
+    }
+  } res=0;
+    res+=minValue;
+    res=Math.round(res)==Math.round(Number.MAX_VALUE)?0:res;
+    console.log("min value is "+res);
     return res;
 }
