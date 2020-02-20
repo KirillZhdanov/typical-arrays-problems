9bb1c64af6733687befe7b37a8d14e5a8302431b
KirillZh <kirill.zhdanov97@gmail.com>
2020-02-20 12:08:39 +0300

    feat: Avg,max,min using arguments array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minValue=Number.MAX_VALUE,res;
+   
+    for (var i = 0; i < arguments.length; i++) {
+      if(minValue>arguments[i])
+      minValue=arguments[i]
+    }
+    res=minValue;
+    Math.round(res)==Math.round(Number.MAX_VALUE)?res=0:res;
+    console.log("min value is "+res);
+    return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxValue=Number.MIN_VALUE,res;
+   
+    for (var i = 0; i < arguments.length; i++) {
+      if(maxValue<arguments[i])
+      maxValue=arguments[i]
+    }
+    res=maxValue;
+    Math.round(res)==Math.round(Number.MIN_VALUE)?res=0:res;
+    console.log("max value is "+res);
+    return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum=0,res;
+    for (var i = 0; i < arguments.length; i++) {
+      sum+= arguments[i];
+    }
+    res=sum/arguments.length;
+    console.log("avg value is "+res);
+    return res;
 }
